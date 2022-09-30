a6d20cfa16da40737bf4e28b8fd0051db2a9c9c9
Victor Damaceno <victordamaceno6489@gmail.com>
2022-09-30 18:01:39 -0300

    create page Buscador de CEP

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
+import { useState } from "react";
+import { FiSearch } from "react-icons/fi";
+import './styles.css'
+
 function App() {
+  const [input, setInput] = useState('')
+
+  function handlesearch() {
+    if (input === '') {
+      alert("Preencha algum CEP!")
+      return;
+    }
+  }
+
   return (
     <div className="container">
       <h1 className='title'>Buscador de CEP</h1>
       <div className='containerInput'>
         <input type="text"
-          placeholder='Digite seu cep' />
+          placeholder='Digite seu cep'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
         <button className='buttonSearch'>
-          Procurar
+          <FiSearch size={25} color='#fff' />
         </button>
       </div>
+      <main className='main'>
+        <h2>CEP: 79000-589</h2>
+        <span>Rua teste Algum</span>
+        <span>Complemento</span>
+        <span>Vila Rosa</span>
+        <span>São Paulo</span>
+      </main>
     </div>
   );
 }
